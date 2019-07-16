97e9e5273203282a05baa2567600ce7b33f1c91b
OnnigPano <onigpano@gmail.com>
2019-07-16 00:23:02 -0300

    storage:link, serviceProviders fixed, migrations sin seeds

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar', 100);
+            $table->string('country', 50);
+            $table->tinyInteger('admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
