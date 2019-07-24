faf8cf2ebfdf7f6c1d8262c93daa274eaf1d49bb
Ciro <ciro_@live.com.ar>
2019-07-23 23:40:00 -0300

    Product Detail View

--- resources/views/front/Product/show.blade.php
+++ resources/views/front/Product/show.blade.php
@@ -5,42 +5,47 @@
 @endsection
 
 @section('mainContent')
-  <br>
-<h1>Show de productos</h1>
-
-<h2> {{ $productFound->name  }} </h2>
-
-<img src="/storage/products/{{ $productFound->image }}" width="300px" style="max-width: 500px;" alt="imagen producto">
-<br><br><br>
-
-
-<h3>Precio: ${{ $productFound->price  }} </h3>
 
 <br>
+<main>
+    <div class="container-detalle">
+      <div class="row">
+        <div class="container-img col-lg-6 col-md-6 col-sm-6 col-12">
+            <img src="/storage/products/{{ $productFound->image }}" width="300px" style="max-width: 500px;" alt="imagen producto">
+        </div>
+        <div class="container-datos-producto col-lg-6 col-md-6 col-sm-6 col-12">
+          <h1> {{ $productFound->name  }} </h1>
+          <p>Descripción: {{ $productFound->description  }} </p>
+          <h3 class="text-success">Precio: ${{ $productFound->price  }} </h3>
+          <br>
+          <div class="container-cat-brand">
+            <h5>Categoria: {{ $productFound->category->name  }} </h5>
+            <h5>Marca: {{ $productFound->brand->name  }} </h5>
+          </div>
 
-<h3>Categoria: {{ $productFound->category->name  }} </h3>
 
-<br>
+          <button class="button-oferta" type="button" name="button">Buy NOW</button>
 
-<h3>Marca: {{ $productFound->brand->name  }} </h3>
+        </div>
+        <div class="container-buttons col-6">
+                <div class="form-inline">
+                    <form  action="/products/{{ $productFound->id }}/edit" method="get">
+                        <button class="btn btn-success mr-2" type="submit">EDITAR</button>
+                    </form>
 
-<br>
+                    <form  action="/products/{{ $productFound->id }}" method="post">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <button class="btn btn-danger" type="submit">ELIMINAR</button>
+                    </form>
+                </div>
+            </div>
 
-<h3>Descripción: {{ $productFound->description  }} </h3>
 
+      </div>
+    </div>
+  </main>
 
-
-<div class="form-inline">
-    <form  action="/products/{{ $productFound->id }}/edit" method="get">
-        <button class="btn btn-success mr-2" type="submit">EDITAR</button>
-    </form>
-
-    <form  action="/products/{{ $productFound->id }}" method="post">
-        @csrf
-        {{ method_field('DELETE') }}
-        <button class="btn btn-danger" type="submit">ELIMINAR</button>
-    </form>
-</div>
 
 
 
