34fe16d0a52ce3bca59a05b50ccafad65792aeb2
OnnigPano <onnigpano@gmail.com>
2019-08-01 23:04:00 -0300

    2 (dos) DOS LINEAS me hacen pushear, DOSSS

--- public/js/validateRegister.js
+++ public/js/validateRegister.js
@@ -1,27 +1,31 @@
-// Capturamos al formulario
-var theForm = document.querySelector('form');
-
-// Obtenemos todos los campos, pero parseamos la colección a un Array
-var formInputs = Array.from(theForm.elements);
-
-// Sacamos la 1er posición del array que es el un <input> hidden del token
-formInputs.shift();
-
-// Sacamos al último elemento que es el <button>
-formInputs.pop();
-
-// Expresión regular para validar solo números
-var regexNumber = /^\d+$/;
-
-// Objeto literal para verificar si un campo tiene error
-var errorsObj = {};
-
-// Recorremos el array y asignamos la validación básica
-formInputs.forEach(function (oneInput) {
-	// A cada campo le asignamos el evento blur y su funcionalidad
-	oneInput.addEventListener('blur', function () {
-		// Pregunto si el campo está vacío (previo trimeo de espacios)
-		if (this.value.trim() === '') {
+window.addEventListener('load', function () {
+	
+	
+	// Capturamos al formulario
+	var theForm = document.querySelector('#form-register');
+	
+	// Obtenemos todos los campos, pero parseamos la colección a un Array
+	var formInputs = Array.from(theForm.elements);
+	
+	// Sacamos la 1er posición del array que es el un <input> hidden del token
+	formInputs.shift();
+	
+	// Sacamos al último elemento que es el <button>
+	formInputs.pop();
+	
+	// Expresión regular para validar solo números
+	var regexNumber = /^\d+$/;
+	
+	// Objeto literal para verificar si un campo tiene error
+	var errorsObj = {};
+	
+	// Recorremos el array y asignamos la validación básica
+	formInputs.forEach(function (oneInput) {
+		// A cada campo le asignamos el evento blur y su funcionalidad
+		oneInput.addEventListener('blur', function () {
+			
+			// Pregunto si el campo está vacío (previo trimeo de espacios)
+			if (this.value.trim() === '') {
 			// Si el campo está vacío, le agrego la clase 'is-invalid'
 			this.classList.add('is-invalid');
 			// Ademas, al <div> con clase 'invalid-feedback' le agremamos el texto de error
@@ -30,16 +34,16 @@ formInputs.forEach(function (oneInput) {
 			errorsObj[this.name] = true;
 		} else {
 			// Cuando el campo NO está vacío
-
+			
 			// Quitamos la clase de error SI existiera
 			this.classList.remove('is-invalid');
 
 			// Si la data es correcta, asignamos esta clase de bootstrap
 			this.classList.add('is-valid');
-
+			
 			// Al mensaje de error le sacamos el texto
 			this.nextElementSibling.innerHTML = '';
-
+			
 			// Si un campo NO tiene error, eliminamos la key del objeto y su valor
 			delete errorsObj[this.name];
 
@@ -53,7 +57,7 @@ formInputs.forEach(function (oneInput) {
 					errorsObj[this.name] = true;
 				}
 			}
-
+			
 			// Validamos el campo rating para verificar que sean solo números
 			if (this.name === 'rating') {
 				if (!regexNumber.test(this.value.trim())) {
@@ -65,12 +69,12 @@ formInputs.forEach(function (oneInput) {
 			}
 		}
 	});
-
+	
 	// oneInput.addEventListener('focus', function () {
-	// 	var inputName = this.name;
-	// 	switch (inputName) {
-	// 		case 'title':
-	// 			console.log('Título de la película inferior a 15 letras');
+		// 	var inputName = this.name;
+		// 	switch (inputName) {
+			// 		case 'title':
+			// 			console.log('Título de la película inferior a 15 letras');
 	// 			break;
 	// 		case 'rating':
 	// 			console.log('Solamente números');
@@ -92,12 +96,14 @@ theForm.addEventListener('submit', function (event) {
 			input.nextElementSibling.innerHTML = 'El campo <b>' + input.getAttribute('data-nombre') + '</b> es obligatorio';
 		}
 	});
-
+	
 	console.log('Campos con errores:', errorsObj);
 	console.log('Cantidad de campos con errores:', Object.keys(errorsObj).length);
-
+	
 	// Si el objeto que contiene los errores NO está vacío evitamos que se SUBMITEE el formulario
 	if (Object.keys(errorsObj).length > 0) {
 		event.preventDefault();
 	}
+});
+
 });
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data" id="FormRegister">
+                    <form id="form-register" method="POST" action="{{ route('register') }}" enctype="multipart/form-data" id="FormRegister">
                         @csrf
 
                         <div class="form-group row">
